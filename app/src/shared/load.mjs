5d0ea0522ff57cab131f18d86ed7ab2e4ff0d801
Tim Brock <tim@jumpingrivers.com>
2024-04-02 17:45:39 +0100

    Add real data to line charts
    Changing area or smoothing will animate the
    changes to the lines

--- app/src/shared/load.mjs
+++ app/src/shared/load.mjs
@@ -1,13 +1,9 @@
-import { json, csv } from 'd3-fetch';
-
-const last = arr => arr[arr.length - 1];
+import { json } from 'd3-fetch';
 
 
 async function load(url) {
   try {
-    const extension = last(url.split('.'));
-    const loader = extension === 'csv' ? csv : json;
-    const data = await loader(url);
+    const data = await json(url);
     return data;
   }
   catch(e) {
